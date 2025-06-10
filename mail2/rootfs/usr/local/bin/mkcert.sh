0e0675b81c69c04dff6e82c25e1337f3c6c15d04
Erik Hilton <erikhilton73@gmail.com>
2025-06-10 20:02:20 +0200

    Fix quotes

--- mail2/rootfs/usr/local/bin/mkcert.sh
+++ mail2/rootfs/usr/local/bin/mkcert.sh
@@ -9,27 +9,27 @@ SSLDIR=${SSLDIR-/etc/ssl/dovecot}
 OPENSSLCONFIG=${OPENSSLCONFIG-dovecot-openssl.cnf}
 
 CERTDIR=$SSLDIR
-KEYDIR=$SSLDIR
+
 
 CERTFILE=$CERTDIR/server.pem
 KEYFILE=$CERTDIR/server.key
 
-if [ ! -d $CERTDIR ]; then
+if [ ! -d "$CERTDIR" ]; then
   echo "$CERTDIR directory doesn't exist"
   exit 1
 fi
 
-if [ -f $CERTFILE ]; then
+if [ -f "$CERTFILE" ]; then
   echo "$CERTFILE already exists, won't overwrite"
   exit 1
 fi
 
-if [ -f $KEYFILE ]; then
+if [ -f "$KEYFILE" ]; then
   echo "$KEYFILE already exists, won't overwrite"
   exit 1
 fi
 
-$OPENSSL req -new -x509 -nodes -config $OPENSSLCONFIG -out $CERTFILE -keyout $KEYFILE -days 365 || exit 2
-chmod 0600 $KEYFILE
+$OPENSSL req -new -x509 -nodes -config "$OPENSSLCONFIG" -out "$CERTFILE" -keyout $KEYFILE -days 365 || exit 2
+chmod 0600 "$KEYFILE"
 echo 
-$OPENSSL x509 -subject -fingerprint -noout -in $CERTFILE || exit 2
+$OPENSSL x509 -subject -fingerprint -noout -in "$CERTFILE" || exit 2
